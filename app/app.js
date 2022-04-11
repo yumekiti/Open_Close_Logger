5c3e2e2257b587648fef8fb817bc4826056c05db
yumekiti <yumekiti120440@gmail.com>
2022-04-12 00:05:06 +0900

    `prettier`

--- app/app.js
+++ app/app.js
@@ -1,43 +1,45 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const bodyParser = require('body-parser');
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 80;
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.get('/' , function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/', async function(req, res){
+app.post("/", async function (req, res) {
   await prisma.status.create({
     data: {
       body: JSON.parse(req.body?.status),
     },
-  })
+  });
 
-  const status = await prisma.status.findMany()
-  io.emit('event', status);
+  const status = await prisma.status.findMany();
+  io.emit("event", status);
 
-  res.send('status update!!')
-})
+  res.send("status update!!");
+});
 
-io.on('connection', async function(socket){
-  const status = await prisma.status.findMany()
-  if(status === []) socket.emit('event', status);
+io.on("connection", async function (socket) {
+  const status = await prisma.status.findMany();
+  if (status === []) socket.emit("event", status);
 
-  socket.on('event', function(status){
-    io.emit('event', status);
+  socket.on("event", function (status) {
+    io.emit("event", status);
   });
 });
 
-http.listen(PORT, function(){
-  console.log('server listening. Port:' + PORT);
+http.listen(PORT, function () {
+  console.log("server listening. Port:" + PORT);
 });
