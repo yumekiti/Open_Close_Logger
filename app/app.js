692c02bbe1cb953926337c16080a2a7510c328da
yumekiti <yumekiti120440@gmail.com>
2022-04-11 19:34:45 +0900

    `add` sqlite

--- app/app.js
+++ app/app.js
@@ -1,19 +1,32 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const app = express();
+const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 80;
+const prisma = new PrismaClient()
 
 app.get('/' , function(req, res){
-    res.sendFile(__dirname+'/index.html');
+  res.sendFile(__dirname+'/index.html');
 });
 
+app.post('/', async function(req, res){
+  await prisma.status.create({
+    data: {
+      body: false,
+    },
+  })
+  
+  const status = await prisma.status.findMany()
+  io.emit('message', status.body);
+})
+
 io.on('connection',function(socket){
-    socket.on('message',function(msg){
-        io.emit('message', msg);
-    });
+  socket.on('message',function(msg){
+    io.emit('message', msg);
+  });
 });
 
 http.listen(PORT, function(){
-    console.log('server listening. Port:' + PORT);
+  console.log('server listening. Port:' + PORT);
 });
