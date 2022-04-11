1319ed3dde80580f9e736f7156690f19c62c86a4
yumekiti <yumekiti120440@gmail.com>
2022-04-11 23:16:01 +0900

    `add` app

--- app/app.js
+++ app/app.js
@@ -1,27 +1,36 @@
 const express = require('express');
-const { PrismaClient } = require('@prisma/client');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 80;
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient()
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.get('/' , function(req, res){
-  res.sendFile(__dirname+'/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/', async function(req, res){
   await prisma.status.create({
     data: {
-      body: true,
+      body: req.body?.status ? true : false,
     },
   })
 
   const status = await prisma.status.findMany()
-  io.emit('event', status.slice(-1)[0].body);
+  io.emit('event', status);
 })
 
-io.on('connection', function(socket){
+io.on('connection', async function(socket){
+  const status = await prisma.status.findMany()
+  socket.emit('event', status);
+
   socket.on('event', function(status){
     io.emit('event', status);
   });
