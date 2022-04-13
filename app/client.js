560d5d3d1e0d890184315328bec1280da0d3f58c
yumekiti <yumekiti120440@gmail.com>
2022-04-13 15:20:10 +0900

    `refactor`

--- app/client.js
+++ app/client.js
@@ -4,7 +4,44 @@ const socket = io();
 // 初期ロードフラグ
 let init = true
 
-// データの受け取り
+// ログの生成
+const newState = (value) => {
+  const element = document.getElementById('history')
+  const li = document.createElement('li')
+
+  // 日付のフォーマット
+  const date = new Date(value.createdAt);
+
+  // ゼロ埋め
+  const zeroPadding = (date) => {
+    return date.toString().padStart(2, "0")
+  }
+
+  // フォーマットされた日付の生成
+  const formatDate = (date)=>{
+    const formatted = date.getFullYear() + "-" + 
+    zeroPadding((date.getMonth() + 1)) + "-" + 
+    zeroPadding(date.getDate()) + "&nbsp;" + 
+    zeroPadding(date.getHours()) + ":" + 
+    zeroPadding(date.getMinutes()) + ":" + 
+    zeroPadding(date.getSeconds());
+    return formatted;
+  }
+
+  // HTMLの追加
+  li.innerHTML = `
+    <div class="card">
+      <p>${formatDate(date)}</p>
+      <div class="detail">
+        <span class="status">${value.body ? "OPEN" : "CLOSE" }</span>
+        <span class="circle" style="background-color: ${value.body ? "blue" : "red" }"></span>
+      </div>
+    </div>
+  `
+  element.prepend(li)
+}
+
+// データの受け取ったとき
 socket.on('event', function(status){
 
   // 南京錠の画像変更
@@ -12,49 +49,13 @@ socket.on('event', function(status){
     '<img src="/images/unlock" alt="開いている時のアイコン" width="300" height="380" />' :
     '<img src="/images/lock" alt="閉まっている時のアイコン" width="300" height="380" />'
 
-  // ログの生成
-  const newState = (value) => {
-    const element = document.getElementById('history')
-    const li = document.createElement('li')
-
-    // 日付のフォーマット
-    const date = new Date(value.createdAt);
-
-    // ゼロ埋め
-    const zeroPadding = (date) => {
-      return date.toString().padStart(2, "0")
-    }
-
-    // フォーマットされた日付の生成
-    const formatDate = (date)=>{
-      const formatted = date.getFullYear() + "-" + 
-      zeroPadding((date.getMonth() + 1)) + "-" + 
-      zeroPadding(date.getDate()) + "&nbsp;" + 
-      zeroPadding(date.getHours()) + ":" + 
-      zeroPadding(date.getMinutes()) + ":" + 
-      zeroPadding(date.getSeconds());
-      return formatted;
-    }
-
-    // HTMLの追加
-    li.innerHTML = `
-      <div class="card">
-        <p>${formatDate(date)}</p>
-        <div class="detail">
-          <span class="status">${value.body ? "OPEN" : "CLOSE" }</span>
-          <span class="circle" style="background-color: ${value.body ? "blue" : "red" }"></span>
-        </div>
-      </div>
-    `
-    element.prepend(li)
-  }
-
+  // 初期ロードかどうか
   if(init){
-    // 初期ロードなら複数ログを生成する
+    // 複数ログを生成
     status.map(value => newState(value))
     init = false
   } else {
-    // 初期ロードでなければ単一ログを生成する
+    // 単一ログを生成
     newState(status.slice(-1)[0])
   }
 })
