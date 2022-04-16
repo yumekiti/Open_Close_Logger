5b392ae978672f7fd851662daa0491dc4c5dc1e2
yumekiti <yumekiti120440@gmail.com>
2022-04-17 07:43:55 +0900

    `same`

--- python/main.py
+++ python/main.py
@@ -1,12 +1,27 @@
 import utime
 from machine import Pin
 
+# 宣言
+sw = Pin(15, Pin.IN, Pin.PULL_UP)
 led = Pin(25, Pin.OUT)
 
+# 変数の初期化
+status = sw.value()
+lock = False
+led.value(status)
+
 while True:
-  led.value(1)
-  print(True)
-  utime.sleep(3)
-  led.value(0)
-  print(False)
-  utime.sleep(3)
+
+  # 読み込み
+  status = sw.value()
+
+  # 変更があった時
+  if status != lock:
+    # 状態を表示
+    print(status)
+    # 状態 True なら光らせる
+    led.value(status)
+
+  lock = status
+
+  utime.sleep(0.50)
