a75c1a83823c7504891d104ef6898a5f0058c81f
yumekiti <yumekiti120440@gmail.com>
2022-05-25 18:10:31 +0900

    `add` port

--- app/app.js
+++ app/app.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 
+// サーバーポートの指定
+const PORT = process.env.PORT || 8080;
+
 // Hello, World! を返す
+app.get("/hello", (req, res) => res.send("Hello, World!"));
 
 // サーバーの実行
 http.listen(PORT, () => {
